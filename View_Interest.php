7e6d0e238fef6114627a20eb4bd352ffa9a67ed4
DINAK LAL THILLANKERI <dinaklal@gmail.com>
2020-12-01 08:49:13 +0530

    Add selection done

--- View_Interest.php
+++ View_Interest.php
@@ -64,13 +64,13 @@
       <div class="row">
         <div  class="col-sm-3 col-md-2 sidebar" >
           <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Home</a></li>
-             <li><a href="add_student.php">Add Student</a></li>
+          <li><a href="index.php">Home</a></li>
+                 <li><a href="add_student.php">Add Student</a></li>
              <li><a href="view_students.php">View Students</a></li>
             <li ><a href="Add_company.php">Add Company </a></li>
             <li><a href="view_company.php">View Company</a></li>
-            <li ><a href="Add_Interest.php">Add Interests</a></li>
-            <li ><a href="View_Interest.php">View Interests</a></li>
+            <li  ><a href="Add_Interest.php">Add Interests</a></li>
+            <li class="active"><a href="View_Interest.php">View Interests</a></li>
             <li ><a href="schedule.php">Make schedule</a></li>
             <li ><a href="Add_Sele.php">Add Selection </a></li>
             <li ><a href="Add_Sele.php">View Selected </a></li>
@@ -81,33 +81,79 @@
                  </ul>
        
        </div>
+    <BR>
+        
+<?Php
 
+		            require_once('db_connect.php');
+        
+					$q="select 
 
+                    s.sid,s.name, s.branch,s.email,c.co_name ,c.place
+                    
+                    from intrested_in_company ci join company c on ci.cid = c.cid  
+                    join student s on s.sid = ci.sid
+                    GROUP by ci.sid, ci.cid
+                    
+                    
+                    ;";
+					$res=mysqli_query($conn,$q);
+					$i=0;
+                    echo "<br>
+              
+          <div class='col-sm-offset-2  table-responsive'>
+            <table class='table table-responsive' style='background-color:white;'>
+              <thead>
+                <tr>
 
-          <div class="row placeholders">
-<?php
+                <th >SID</th>
+                  <th >Student Name</th>
+                  <th>Branch</th>
+                  <th>Email</th>
+                  <th>Company</th>
+                  <th>Place</th>
+                </tr>
+              </thead>
+              <tbody>";
+					while($row=mysqli_fetch_array($res))
+					{
+						$i++;
+                        $id=$row['sid'];
+                        $name=$row['name'];
+                        $c_name=$row['co_name'];
+						$place=$row['place'];
+						
+						$email =$row['email'];
+						$branch =$row['branch'];
+            
+						echo "<tr  class='clickable-row' data-href='mem_home.php?id=$id'>
+                     
+                 <td>S$id</td>
+                <td> $name</td>
+                  <td>$branch</td>
+                  <td>$email</td>        
+                  <td>$c_name</td>    
+                  <td>$place</td>
+      
+                </tr>";
+					}
+                                      
+                                 
 
-require_once('db_connect.php');
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  
-  
-  $sql = "SELECT name FROM Student";
-  $result = $conn->query($sql);
-
-  
 ?>
-</div>
 
-<div class="footer">
-            <h5 class="page-header">Powered By : DINAK LAL V </h5>
-  <!-- Bootstrap core JavaScript
+               
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/docs.min.js"></script>
   </body>
